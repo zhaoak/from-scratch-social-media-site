c6fc15527726b274f83ca2b760b140763564953a
Allie Zhao <allie.k.zhao@gmail.com>
2022-12-05 15:47:34 -0800

    added basic nav bar rendering, no styling on it yet

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 import { fetchAllProfiles } from './fetch-utils.js';
-import { renderProfileCard } from './render-utils.js';
+import { renderProfileCard, renderNavBarContents } from './render-utils.js';
 
 /* Get DOM Elements */
 const profilesListEl = document.getElementById('profiles-list');
+const navSectionEl = document.querySelector('nav');
 
 /* State */
 let profiles = [];
@@ -15,6 +16,9 @@ window.addEventListener('load', async () => {
     profiles = await fetchAllProfiles();
     displayProfileList();
     console.log(profiles);
+
+    // for now, pretend logged in user is first one in profiles array
+    navSectionEl.append(renderNavBarContents(profiles[0]));
 });
 
 /* Display Functions */
--- render-utils.js
+++ render-utils.js
@@ -15,3 +15,39 @@ export function renderProfileCard(profile) {
     profileContainer.append(avatarImageEl, usernameEl, popularityScoreEl);
     return profileContainer;
 }
+
+// pass in logged in user's profile as argument
+export function renderNavBarContents(profile) {
+    const navBarContents = document.createElement('div');
+    const profileListPageLink = document.createElement('a');
+    const editProfilePageLink = document.createElement('a');
+    const currentUserUsernameLink = document.createElement('a');
+    const currentUserAvatar = document.createElement('img');
+    const logoutLink = document.createElement('a');
+    const separatorChar = document.createElement('span');
+
+    profileListPageLink.href = '../index.html';
+    profileListPageLink.textContent = 'All Profiles';
+    editProfilePageLink.href = '../edit/';
+    editProfilePageLink.textContent = 'Edit Your Profile';
+    currentUserUsernameLink.href = `../detail/index.html?id=${profile.id}`;
+    currentUserUsernameLink.textContent = `${profile.user_name}`;
+    // making an anchor tag to put avatar image inside of so avatar can act as a link
+    const avatarLinkWrapper = document.createElement('a');
+    avatarLinkWrapper.href = `../detail/index.html?id=${profile.id}`;
+    currentUserAvatar.src = profile.avatar_url;
+    avatarLinkWrapper.append(currentUserAvatar);
+    separatorChar.textContent = '|';
+
+    // add css classes to nav bar elements
+
+    navBarContents.append(
+        profileListPageLink,
+        separatorChar,
+        editProfilePageLink,
+        separatorChar,
+        currentUserUsernameLink,
+        avatarLinkWrapper
+    );
+    return navBarContents;
+}
